52857f0f24515b9cc258d019389d0c3fcf16fcad
Levis Libra <levislibra@hotmail.com>
2019-12-11 16:08:44 -0300

    Update

--- __openerp__.py
+++ __openerp__.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "financiera_whatsapp",
+    'name': "Whatsapp Notificaiones para Financiera",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Notificaiones por medio de Whatsapp con posibilidad de respuestas.""",
 
     'description': """
-        Long description of module's purpose
+        Moudulo Whatsapp Notificaciones para Financiera.
+        * Notificaciones para cobranza preventiva.
+        * Notificaciones de mora.
+        * Notificaciones de punitorios.
+        * Validacion de numero de movil.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Librasoft",
+    'website': "https://libra-soft.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'finance',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
