63701025b928cc6edec31c93c0cce4f9498f6515
Levis Libra <levislibra@hotmail.com>
2019-12-27 19:38:26 -0300

    Update

--- models/models.py
+++ models/models.py
@@ -10,8 +10,40 @@ class FinancieraWhatsappConfig(models.Model):
 	name = fields.Char('Nombre')
 	account_sid = fields.Char('SID de cuenta')
 	auth_token = fields.Char('Token')
+	number_send = fields.Char("Numero Twilio", default="+")
+	number_received_test = fields.Char("Numero Recepcion para Test", default="+")
+	body_send_test = fields.Text("Texto a enviar")
+	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.config'))
+	# Mensajes
+	# Aviso preventivo
+	preventivo_body = fields.Text('Mensaje')
+	preventivo_when_send = fields.One2many('financiera.wa.send.when', 'preventivo_when_send_id', 'Cuando enviar?')
+	# preventivo_who_send = fields.One2many('financiera.wa.send.who', 'preventivo_who_send_id', 'A quien enviar?')
+
+	@api.one
+	def test_connection(self):
+		client = Client(self.account_sid, self.auth_token)
+		message = client.messages.create(
+			body=self.body_send_test,
+			from_='whatsapp:'+self.number_send,
+			to='whatsapp:'+self.number_received_test
+		)
+
+class FinancieraWhatsappSendWhen(models.Model):
+	_name = 'financiera.wa.send.when'
+
+	preventivo_when_send_id = fields.Many2one('financiera.wa.config')
+	send_type = fields.Selection([('dia_especifico', 'Dia especifico'), ('dia_antes_vencimiento', 'Dias antes del vencimiento'), ('dia_despues_vencimiento', 'Dias despues del vencimiento')], 'Tipo de envio')
+	date = fields.Integer('Dia/Dias')
 	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.config'))
 
+# class FinancieraWhatsappSendWho(models.Model):
+# 	_name = 'financiera.wa.send.who'
+
+# 	preventivo_who_send_id = fields.Many2one('financiera.wa.config')
+# 	send_type = fields.Selection([('dia_especifico', 'Dia especifico'), ('dia_antes_vencimiento', 'Dias antes del vencimiento'), ('dia_despues_vencimiento', 'Dias despues del vencimiento')], 'Tipo de envio')
+# 	date = fields.Integer('Dia/Dias')
+# 	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.config'))
 
 
 class ExtendsResCompany(models.Model):
