d2e5967c04521f89992471162ec0b0fa95671ca6
Levis Libra <levislibra@hotmail.com>
2020-01-03 17:26:13 -0300

    Update

--- models/models.py
+++ models/models.py
@@ -16,9 +16,9 @@ class FinancieraWhatsappConfig(models.Model):
 	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.config'))
 	# Mensajes
 	# Aviso preventivo
-	preventivo_body = fields.Text('Mensaje')
-	preventivo_when_send = fields.One2many('financiera.wa.send.when', 'preventivo_when_send_id', 'Cuando enviar?')
-	preventivo_who_send = fields.One2many('financiera.wa.send.who', 'preventivo_who_send_id', 'A quien enviar?')
+	# preventivo_body = fields.Text('Mensaje')
+	# preventivo_when_send = fields.One2many('financiera.wa.send.when', 'preventivo_when_send_id', 'Cuando enviar?')
+	# preventivo_who_send = fields.One2many('financiera.wa.send.who', 'preventivo_who_send_id', 'A quien enviar?')
 
 	@api.one
 	def test_connection(self):
@@ -29,10 +29,46 @@ class FinancieraWhatsappConfig(models.Model):
 			to='whatsapp:'+self.number_received_test
 		)
 
+class FinancieraWhatsappMessage(models.Model):
+	_name = 'financiera.wa.message'
+
+	name = fields.Char('Nombre')
+	active = fields.Boolean('Activo')
+	state = fields.Selection([('borrador', 'Borrador'), ('aprobado', 'Aprobado'), ('rechazado', 'Rechazado')], 'Estado', default='borrador')
+	body = fields.Text('Mensaje')
+	body_word_replace = fields.Text('Palabras para reemplazar', default="""
+	Cliente:
+	* PRESTAMOS
+		- prestamo_monto: monto solicitado
+	* Cuotas
+		- cuota_proximo_vencimiento: Proximo vencimiento y monto
+		- cuota_vencimientos_montos: Vencimientos y montos del mes
+		- cuota_vencidas_montos: Vencimeintos y montos vencidos
+	* Generales:
+		- monto_deuda_vencida: Monto deuda vencida
+		- deuda_total: Deuda total
+	""")
+	when_send = fields.One2many('financiera.wa.send.when', 'when_send_id', 'Cuando enviar?')
+	who_send = fields.One2many('financiera.wa.send.who', 'who_send_id', 'A quien enviar?')
+	message_ids = fields.One2many('financiera.wa.message.send', 'wa_message_id', 'Mensajes enviados')
+	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.message'))
+
+	@api.one
+	def aprobar(self):
+		self.state = 'aprobado'
+
+	@api.one
+	def borrador(self):
+		self.state = 'borrador'
+
+	@api.one
+	def rechazar(self):
+		self.state = 'rechazado'
+
 class FinancieraWhatsappSendWhen(models.Model):
 	_name = 'financiera.wa.send.when'
 
-	preventivo_when_send_id = fields.Many2one('financiera.wa.config')
+	when_send_id = fields.Many2one('financiera.wa.message')
 	send_type = fields.Selection([('dia_especifico', 'Dia especifico'), ('dia_antes_vencimiento', 'Dias antes del vencimiento'), ('dia_despues_vencimiento', 'Dias despues del vencimiento')], 'Tipo de envio')
 	date = fields.Integer('Dia/Dias')
 	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.config'))
@@ -40,13 +76,25 @@ class FinancieraWhatsappSendWhen(models.Model):
 class FinancieraWhatsappSendWho(models.Model):
 	_name = 'financiera.wa.send.who'
 
-	preventivo_who_send_id = fields.Many2one('financiera.wa.config')
+	who_send_id = fields.Many2one('financiera.wa.message')
 	send_who = fields.Selection([
-		('cuota_activa', 'Cliente con cuota activa'),
-		('cuota_preventiva', 'Cliente con cuota en preventiva')],
+		('cuotas_activa', 'Cliente con al menos N cuotas activas'),
+		('cuotas_vencidas', 'Cliente con al menos N cuotas vencidas'),
+		('cuotas_preventiva', 'Cliente con al menos N cuotas en preventiva y no mas de M cuotas vencidas')],
 		'Tipo de envio')
+	n = fields.Integer("N")
+	m = fields.Integer("M")
 	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.config'))
 
+class FinancieraWhatsappMessageSend(models.Model):
+	_name = 'financiera.wa.message.send'
+
+	_order = 'id desc'
+	partner_id = fields.Many2one('res.partner','Cliente')
+	wa_message_id = fields.Many2one('financiera.wa.message', 'Estructura de mensaje')
+	# state = fields.Selection([('enviado', 'Enviado'), ('recibido', 'Recibido'), ('rechazado', 'Rechazado')], 'Estado', default='enviado')
+	body = fields.Text('Mensaje')
+	company_id = fields.Many2one('res.company', 'Empresa', default=lambda self: self.env['res.company']._company_default_get('financiera.whatsapp.message'))
 
 class ExtendsResCompany(models.Model):
 	_inherit = 'res.company'
