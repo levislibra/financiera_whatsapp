f234459ece0d581e749eec9d6f0cba718039e86c
Levis <levislibra@hotmail.com>
2021-04-11 22:11:56 -0300

    Add button whatsapp in partner contact

--- models/extends_res_partner.py
+++ models/extends_res_partner.py
@@ -19,3 +19,20 @@ class ExtendsResPartner(models.Model):
 			}
 		else:
 			raise UserError("El celular no esta cargado.")
+
+class ExtendsResPartnerContaco(models.Model):
+	_name = 'res.partner.contacto'
+	_inherit = 'res.partner.contacto'
+
+	@api.multi
+	def button_open_whatsapp(self):
+		if self.movil:
+			return {
+				'name'     : 'Whatsapp',
+				'res_model': 'ir.actions.act_url',
+				'type'     : 'ir.actions.act_url',
+				'target'   : 'new',
+				'url'      : 'https://wa.me/+549' + self.movil + '?text=Hola ' + self.partner_id.name
+			}
+		else:
+			raise UserError("El celular no esta cargado.")
